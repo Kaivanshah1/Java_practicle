10b7157c7edb6f8530712ca4d41782b7457e604f
Kaivanshah1 <110343220+Kaivanshah1@users.noreply.github.com>
2022-07-31 21:39:18 +0530

    Create Prac2.java

---
+++ Prac2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if(s.charAt(0) == 'o' && s.charAt(1) == 'z')
+		{
+			System.out.println("oz");
+		}
+		else if(s.charAt(0) == 'o')
+		{
+			System.out.println('o');
+		}
+		else if(s.charAt(1) == 'z')
+		{
+			System.out.println('z');
+		}
+    }
+}
